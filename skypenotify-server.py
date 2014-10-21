29dfc8920d2d2a7a96d09f280260e95616a3ba40
harryprotist <benmS@gmail.com>
2014-10-21 16:52:53 -0700

    starts notify server class

--- skypenotify-server.py
+++ skypenotify-server.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python2
 import Skype4Py
+import sys
+import socket
+
+def log(msg):
+	print >> sys.stderr, str(msg)
+
+class NotifyServer:
+
+	def __init__(self, client, port):
+		self.skype = client
+		self.port  = port	
+
+	def initSocket(self):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.bind(self.port)
+		return s
+	
+	def run(self, frequency):
+		
+	
+	def getUnreadCount(self):
+		return	self.skype.MissedMessages.Count +
+			self.skype.MissedCalls.Count +
+			self.skype.MissedChats.Count +
+			self.skype.MissedVoicemails.Count	
+
+
+
 
 skype = Skype4Py.Skype()
 skype.Attach()
